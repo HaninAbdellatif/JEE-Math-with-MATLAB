3ff3139f8e27d9e8c162c0ee1778192d22d1674a
HaninAbdellatif <166175876+HaninAbdellatif@users.noreply.github.com>
2025-08-13 22:26:05 +0300

    Update Area.m

--- Area.m
+++ Area.m
@@ -39,3 +39,27 @@ y2 = 2*x - x.^2;     % elementwise power for x^2
 diff = y1 - y2;
 area = trapz(x, abs(diff));
 
+%11
+
+%% exploring the graph
+y1 = linspace(-10, 10, 400);
+ x1 = y1.^2 / 4;
+x2 = linspace(-10, 10, 400); 
+y2 = x2.^2 / 4; 
+figure;
+plot(x1, y1, 'r', 'LineWidth', 2); hold on;
+plot(x2, y2, 'b', 'LineWidth', 2);
+plot([4 4], [-10 10], 'g--', 'LineWidth', 2);
+plot([-10 10], [4 4], 'k--', 'LineWidth', 2);
+ax = gca; ax.XAxisLocation = 'origin'; ax.YAxisLocation = 'origin';
+>> % areas
+>> y1 = linspace(0, 4, 1000);
+ x1 = y1.^2 / 4;
+>> x2 = sqrt(4*y1);
+>> area1 = trapz(y1, x1);
+>> area2 = trapz(y1, x2);
+>> y2 = linspace(0, 4, 1000);
+>> x3 = y2.^2 / 4;
+>> area3 = trapz(y2, x3);
+
+
