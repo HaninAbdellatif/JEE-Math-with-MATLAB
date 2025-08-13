3cfae40d0baf41fe8110c9b87b2e558b39eef23d
HaninAbdellatif <166175876+HaninAbdellatif@users.noreply.github.com>
2025-08-13 03:43:52 +0300

    Update Area.m

--- Area.m
+++ Area.m
@@ -1,3 +1,4 @@
+% 5
 syms x
 y1 = 2*x - x^2;
 y2 = -x; 
@@ -5,3 +6,14 @@ sol = solve(y1 == y2, x);
 sol = double(sol);
 diff_abs = abs(y2 - y1);
 area = double(int(diff_abs, x, sol(1), sol(2)))
+
+%6
+yms x
+y1 = 2*x - x^2;
+y2 = -x; 
+sol = solve(y1 == y2, x);
+sol = double(sol);
+diff_abs = abs(y2 - y1);
+area = double(int(diff_abs, x, sol(1), sol(2)))
+
+
